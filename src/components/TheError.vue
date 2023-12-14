8e8c3e5407c978c9bc2debb587010a0654c139aa
skhrvg <skhrvg@gmail.com>
2023-12-14 14:41:51 +0300

    fix: error z-index

--- src/components/TheError.vue
+++ src/components/TheError.vue
@@ -12,7 +12,7 @@ const { error } = storeToRefs(useVPNStore())
   >
     <div
       v-if="error"
-      class="fixed top-0 inset-x-0 text-center px-6 py-2 bg-red-500 text-xs cursor-pointer duration-200"
+      class="fixed top-0 inset-x-0 text-center px-6 py-2 bg-red-500 text-xs cursor-pointer duration-200 z-20"
       @click="error = ''"
     >
       {{ error }}
